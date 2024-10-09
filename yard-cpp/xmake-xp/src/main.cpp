bc3e34bcf188dd517c63de72390ca2e4e3de951f
utensil <utensilcandel@gmail.com>
2024-10-09 19:16:27 +0800

    Try xmake with boost and sokol

--- yard-cpp/xmake-xp/src/main.cpp
+++ yard-cpp/xmake-xp/src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #define STB_DS_IMPLEMENTATION
 #include "stb_ds.h"
+#include <boost/math/quaternion.hpp>
+#define SOKOL_IMPL
+#include "sokol_time.h"
 
 int main(int argc, char** argv) {
+    // use sokol_time
+    stm_setup();
+    uint64_t start = stm_now();
+
     // use stb_ds
     int* arr = NULL;
     arrput(arr, 1);
@@ -13,6 +20,13 @@ int main(int argc, char** argv) {
         std::cout << arr[i] << std::endl;
     }
 
-    std::cout << "hello world!" << std::endl;
+    // use boost quaternion
+    boost::math::quaternion<double> q1(1, 2, 3, 4);
+    boost::math::quaternion<double> q2(5, 6, 7, 8);
+    boost::math::quaternion<double> q3 = q1 * q2;
+    std::cout << q3 << std::endl;
+
+    uint64_t elapsed = stm_since(start);
+    std::cout << "elapsed: " << stm_sec(elapsed) << std::endl;
     return 0;
 }
--- yard-cpp/xmake-xp/xmake.lua
+++ yard-cpp/xmake-xp/xmake.lua
@@ -1,11 +1,16 @@
 add_rules("mode.debug") -- , "mode.release")
 
 add_requires("stb")
+add_requires("boost")
+add_requires("sokol")
 
 target("xmake-xp")
     set_kind("binary")
     add_files("src/*.cpp")
     add_packages("stb")
+    add_packages("boost")
+    add_packages("sokol")
+    set_languages("c++20")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
