02d0f91b49410c900e4c5522ff088306e3c777c4
utensil <utensilcandel@gmail.com>
2024-10-09 16:37:59 +0800

    Use `xp` as postfix for explorational projects, not `test`

--- playground/cubecl-xp/tests/test_gelu.rs
+++ playground/cubecl-xp/tests/test_gelu.rs
@@ -2,7 +2,7 @@
 #[test]
 fn test() {
     // #[cfg(feature = "wgpu")]
-    let result = cubecl_test::gelu_launch::<cubecl::wgpu::WgpuRuntime>(&Default::default());
+    let result = cubecl_xp::gelu_launch::<cubecl::wgpu::WgpuRuntime>(&Default::default());
     // pretty_assertions::assert_eq!(vec![-0.1587, 0.0000, 0.8413, 5.0000], result);
     // test float equality for result
     for (a, b) in result.iter().zip(vec![-0.1587, 0.0000, 0.8413, 5.0000].iter()) {
