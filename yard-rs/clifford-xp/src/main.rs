7f3de2613264b708b010907e03b9f0a3979a45d5
utensil <utensilcandel@gmail.com>
2025-06-25 16:49:35 +0800

    refactor(clifford-xp): move tests to tests/ directory

--- yard-rs/clifford-xp/src/main.rs
+++ yard-rs/clifford-xp/src/main.rs
@@ -1,12 +1,3 @@
 fn main() {
     println!("Hello, world!");
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_main_output() {
-        // This is a simple test to verify the project structure
-        assert!(true);
-    }
-}
---
+++ yard-rs/clifford-xp/tests/main_test.rs
@@ -0,0 +1,5 @@
+#[test]
+fn test_main_output() {
+    // This is a simple test to verify the project structure
+    assert!(true);
+}
